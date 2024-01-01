eb26d04a58bf6348085f670323e7050ce0b4fbef
Scotty <73749875+intondiscott@users.noreply.github.com>
2024-01-01 15:02:19 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# WT-32-SC-01-DEV
+<h1 style="text-align:center">WT-32-SC-01-DEV</h1>
+<img src="IMG_2985.jpg"  style="width:50%;margin-left:25%; margin-right:75%"/>
--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,6 @@
 
 LGFX tft;
 
-#define screenWidth 480
-#define screenHeight 320
-
-static lv_disp_draw_buf_t draw_buf;
-static lv_color_t buf[screenWidth * 10];
-
 // headers
 void setupLvgl();
 static void drawUI();
@@ -65,12 +59,17 @@ void my_touch_read(lv_indev_drv_t *indev_driver, lv_indev_data_t *data)
 
 void setupLvgl()
 {
+  const int SCREEN_WIDTH = 480;
+  const int SCREEN_HEIGHT = 320;
+
+  static lv_disp_draw_buf_t draw_buf;
+  static lv_color_t buf[SCREEN_WIDTH * 10];
   lv_init();
-  lv_disp_draw_buf_init(&draw_buf, buf, NULL, screenWidth * 10);
+  lv_disp_draw_buf_init(&draw_buf, buf, NULL, SCREEN_WIDTH * 10);
   static lv_disp_drv_t disp_drv;
   lv_disp_drv_init(&disp_drv);
-  disp_drv.hor_res = screenWidth;
-  disp_drv.ver_res = screenHeight;
+  disp_drv.hor_res = SCREEN_WIDTH;
+  disp_drv.ver_res = SCREEN_HEIGHT;
   disp_drv.flush_cb = my_disp_flush;
   disp_drv.draw_buf = &draw_buf;
   lv_disp_drv_register(&disp_drv);
@@ -83,29 +82,42 @@ void setupLvgl()
 
 static void drawUI()
 {
-  lv_obj_t *main_screen, *btn, *label;
-  lv_style_t red;
-  lv_style_init(&red);
-  lv_style_set_bg_color(&red, LV_COLOR_MAKE(255, 0, 0));
+  lv_obj_t *main_screen, *txtArea, *keyboard;
+  static lv_style_t txtStyle;
   main_screen = lv_scr_act();
-  btn = lv_btn_create(main_screen);
-  label = lv_label_create(btn);
-  lv_obj_center(btn);
-  lv_obj_add_style(btn, &red, LV_STATE_DEFAULT);
-  lv_label_set_text(label, "my button");
-  lv_obj_add_event_cb(btn,handleClick,LV_EVENT_ALL,NULL);
+
+  ////////////////////////////////////////////////////////
+  // CSS-LIKE STYLES                                   //
+  //////////////////////////////////////////////////////
+  lv_style_init(&txtStyle);
+  lv_style_set_shadow_width(&txtStyle, 8);
+  lv_style_set_shadow_spread(&txtStyle, 5);
+  lv_style_set_shadow_color(&txtStyle, lv_palette_main(LV_PALETTE_INDIGO));
+  ////////////////////////////////////////////////////////
+  // creating text area                                //
+  //////////////////////////////////////////////////////
+  txtArea = lv_textarea_create(main_screen);
+  lv_obj_add_style(txtArea, &txtStyle, 0);
+  lv_obj_align(txtArea, LV_ALIGN_TOP_MID, 0, 10);
+  lv_obj_set_size(txtArea, 410, 140);
+  
+
+  //////////////////////////////////////////////////////
+  // creating keyboard                               //
+  ////////////////////////////////////////////////////
+  keyboard = lv_keyboard_create(main_screen);
+  lv_keyboard_set_textarea(keyboard, txtArea);
 }
 
 static void handleClick(lv_event_t *event)
 {
-  
+
   lv_event_code_t code = lv_event_get_code(event);
   lv_obj_t *btn = lv_event_get_current_target(event);
   if (code == LV_EVENT_CLICKED)
   {
-    lv_obj_t *label = lv_obj_get_child(btn,0);
-    
+    lv_obj_t *label = lv_obj_get_child(btn, 0);
+
     lv_label_set_text(label, "Clicked!!!");
-    
   }
 }
